656e4a4456932d58d4dd72f25f19dbea86e1dabc
Brian A. Danielak <brian@briandk.com>
2014-11-30 14:30:29 -0700

    Address JSLint style errors

--- javascripts/transdown.js
+++ javascripts/transdown.js
@@ -1,19 +1,19 @@
 var transdown = {};
 
-transdown.updatePreviewAfterEachKeypress = function() {
-    $('#text-to-transdownify').keyup(transdown.renderTranscriptPreview)
-}
+transdown.updatePreviewAfterEachKeypress = function () {
+    $('#text-to-transdownify').keyup(transdown.renderTranscriptPreview);
+};
 
-transdown.renderTranscriptPreview = function() {
-    textToTransdownify = document.getElementById('text-to-transdownify').value;
-    outputText = transdown.transdownify(textToTransdownify);
+transdown.renderTranscriptPreview = function () {
+    var textToTransdownify = document.getElementById('text-to-transdownify').value;
+    var outputText = transdown.transdownify(textToTransdownify);
     document.getElementById('live-preview').innerHTML = outputText;
     console.log("render");
 };
 
-transdown.transdownify = function(text) {
-    return(text);
-}
+transdown.transdownify = function (text) {
+    return (text);
+};
 
 transdown.updatePreviewAfterEachKeypress();
 transdown.renderTranscriptPreview();
