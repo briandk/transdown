b45bc8b6ee8e9feaaa1b45bafdec2302feeadfe9
Brian A. Danielak <brian@briandk.com>
2014-12-01 18:10:58 -0700

    Define data spec for transcript

---
+++ javascripts/transdown-specification.js
@@ -0,0 +1,32 @@
+/*
+A transcript is a collection of episodes.
+
+And episode has: 
+
+- an episode title (optional)
+- a collection of turns
+
+A turn consists of
+
+- A timestamp (optional)
+- A speaker-name
+- Speech
+- Accompanying media(?)
+*/
+
+
+var transcript = {
+    episodes: [],
+}
+
+var episode = {
+    title: "",
+    turns: [],
+}
+
+var turn = {
+    timestamp: "",
+    speakerName: "",
+    speech: "",
+    accompanyingMedia: "",
+}
