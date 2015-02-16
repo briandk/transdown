bf8b4558198e1166faf7c52e88ce09a0babadab4
Brian A. Danielak <brian@briandk.com>
2015-02-15 18:22:46 -0700

    write raw inqscribe transcript to stdout
    Using the node `readline` module, whose stability is rated at a 2/5

--- javascripts/renderInqscribePreviewInMarked.js
+++ javascripts/renderInqscribePreviewInMarked.js
@@ -1,21 +1,37 @@
+// Directory variables
+var pathToTemplate = '/Users/briandanielak/Dropbox/dev/transdown/transcriptTemplate.handlebars';
+
 // load modules
 var Handlebars = require('handlebars');
 var fs = require('fs');
 var transdown = require('./transdown.js');
- 
+var readline = require('readline');
 
-// transdown stuff
+// compile template
 var template = Handlebars.compile(
     fs.readFileSync(
-        '../transcriptTemplate.handlebars', 
-        encoding='utf8'
+        pathToTemplate,
+        encoding = 'utf8'
     )
-)
-
-var text = '[33:33] Rebecca: and then my thinking at least, is you should be able to, um, say that "star p of i" /mmhmm/ equals, uh, the title, and then you just do i++, so then it’ll {{move to the next one}} ![makes looping gesture with left hand][1] /OK/';
-
-var output = template(
-    transdown.transdownify(text)
 );
- 
-console.log(output);
+
+var processInputFromInqscribe = function (text) {
+    "use strict";
+    return (text);
+};
+
+// Reading from stdin
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', function (line) {
+    var inqscribeTranscriptDataPattern = /^text=(.*)/;
+    
+    if (inqscribeTranscriptDataPattern.test(line) === true) {
+        processInputFromInqscribe(line);
+        console.log("LINE " + line);
+    }
+});
