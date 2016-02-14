31580a741b950695a4b6d2f44dc19dfe991c7f92
Brian Danielak <brian@briandk.com>
2016-02-14 16:35:14 -0500

    Delete unnecessary node modules
    Add notes on how to compile the handlebars template

--- javascripts/renderInqscribePreviewInMarked.js
+++ javascripts/renderInqscribePreviewInMarked.js
@@ -1,7 +1,10 @@
+// To compile the handlebars template for this function:
+// handlebars transcriptTemplate.handlebars -c ../bower_components/handlebars/handlebars.js > javascripts/template-node.js
+
 // load modules
 var transdown = require('./transdown.js');
 var readline = require('readline');
-var Handlebars = require('../bower_components/handlebars/handlebars.js');
+var Handlebars = require('../bower_components/handlebars/handlebars');
 require('./template-node.js');
 
 
@@ -9,6 +12,7 @@ var processInputFromInqscribe = function (text, pattern) {
     "use strict";
     text = text.replace(pattern, "");
     text = text.replace(/\\r/g, "\n");
+    text = text.replace(/\\e/g, "="); // Inqscribe handles equals signs weirdly
     text = transdown.transdownify(text);
     text = Handlebars.templates.transcriptTemplate(text);
     return (text);
